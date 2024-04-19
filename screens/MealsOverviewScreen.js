32996828a6eab2572b3b47e4680d2739f33e56d7
Raul Ceron <receronp@gmail.com>
2024-04-19 12:40:51 -0600

    adds functionality of categories screen

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,53 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+
+import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
+
+function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) != -1;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({ title: categoryTitle });
+  }, [catId, navigation]);
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
