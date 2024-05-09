7f324e86dd8df7bdade55b86847b7d35c1552c28
Raul Ceron <receronp@gmail.com>
2024-05-09 16:30:33 -0600

    adds auth context token mgmt

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import AuthContent from "../components/auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { login } from "../util/auth";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
-    setIsAuthenticating(false);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not log you in, please check your credentials or try again later!"
+      );
+      setIsAuthenticating(false);
+    }
   }
 
   if (isAuthenticating) {
